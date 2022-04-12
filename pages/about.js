71faef55278ace9bd193442152d65082458ce5e0
Osaze-Omobude Love <loveomobude23@gmail.com>
2022-04-12 11:48:37 +0100

    PracticeCommit

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto
+          explicabo perspiciatis similique beatae amet velit pariatur nobis
+          inventore aperiam repudiandae?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
